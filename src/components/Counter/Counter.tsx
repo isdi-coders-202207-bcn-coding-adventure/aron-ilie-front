c1948ee761770f1e96091a16dfca97d6c0e69908
Aron Ilie <aronilie.code@gmail.com>
2022-08-22 15:56:45 +0200

    Refactorized Counter component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,19 +2,19 @@ const Counter = (): JSX.Element => {
   return (
     <ul>
       <li>
-        <span className="days"></span>
+        <span className="counter-number">02</span>
         <span>days</span>
       </li>
       <li>
-        <span className="hours"></span>
+        <span className="counter-number">20</span>
         <span>hours</span>
       </li>
       <li>
-        <span className="minutes"></span>
+        <span className="counter-number">34</span>
         <span>minutes</span>
       </li>
       <li>
-        <span className="seconds"></span>
+        <span className="counter-number">09</span>
         <span>seconds</span>
       </li>
     </ul>
