c4436b432066fa12550b8a92df97253e1dd0e860
Aron Ilie <aronilie.code@gmail.com>
2022-08-24 13:59:10 +0200

    Tested Counter component

---
+++ src/components/Counter/Counter.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import Counter from "./Counter";
+
+describe("Given a Counter component", () => {
+  describe("When it is renderized", () => {
+    test("Then it should show the countdown numbers", () => {
+      render(
+        <Provider store={store}>
+          <Counter />
+        </Provider>
+      );
+
+      const days = screen.getByText("days");
+      const hours = screen.getByText("hours");
+      const minutes = screen.getByText("minutes");
+      const seconds = screen.getByText("seconds");
+
+      expect(days).toBeInTheDocument();
+      expect(hours).toBeInTheDocument();
+      expect(minutes).toBeInTheDocument();
+      expect(seconds).toBeInTheDocument();
+    });
+  });
+});
